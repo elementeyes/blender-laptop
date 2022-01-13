c9a59789a9cde1701c419cd5ca1c92d51aa97706
element <[elementeyes1337@gmail.com]>
2022-01-13 20:55:40 +0200

    first commit

---
+++ src/client/client.ts
@@ -0,0 +1,96 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
+import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader'
+import Stats from 'three/examples/jsm/libs/stats.module'
+
+
+
+
+const scene = new THREE.Scene()
+
+
+scene.background = new THREE.Color( 0xffaff );
+const spotLight = new THREE.SpotLight( 0xffffff );
+spotLight.position.set( -99, 30, -90 );
+scene.add( spotLight );
+
+const spotLight2 = new THREE.SpotLight( 0xffffff );
+spotLight2.position.set( 99, 30, 90 );
+scene.add( spotLight2 );
+
+
+
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+)
+camera.position.z = 3
+
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+
+document.body.appendChild(renderer.domElement)
+
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.enableDamping = true
+
+const mtlLoader = new MTLLoader()
+mtlLoader.load(
+    'assets/laptop.mtl',
+    (materials) => {
+        materials.preload()
+
+        const objLoader = new OBJLoader()
+        objLoader.setMaterials(materials)
+        objLoader.load(
+            'assets/laptop.obj',
+            (object) => {
+                scene.add(object)
+            },
+            (xhr) => {
+                console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+            },
+            (error) => {
+                console.log('An error happened')
+            }
+        )
+    },
+    (xhr) => {
+        console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+    },
+    (error) => {
+        console.log('An error happened')
+    }
+)
+
+
+
+window.addEventListener('resize', onWindowResize, false)
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    render()
+}
+
+const stats = Stats()
+document.body.appendChild(stats.dom)
+
+function animate() {
+    requestAnimationFrame(animate)
+
+    controls.update()
+
+    render()
+
+    stats.update()
+}
+
+function render() {
+    renderer.render(scene, camera)
+}
+
+animate()
